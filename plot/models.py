20c97278629af37533f98096f0a37e1d81c1c04c
djsmedes <djsmedes@gmail.com>
2018-08-22 17:40:24 -0400

    created Campaign model

--- plot/models.py
+++ plot/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.conf import settings
+
+from core.models import TenantModel
 
 
-class Campaign(models.Model):
-    class Meta:
-        abstract = True
+class Campaign(TenantModel):
+    creation_date = models.DateField(auto_now_add=True)
+    gm_set = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        editable=False,
+        related_name='campaigns_gm_of',
+    )
+    player_set = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='campaigns_player_of',
+    )
