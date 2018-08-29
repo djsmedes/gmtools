fc9d516f257680a805a697d687f07da6ef5b2ae2
djsmedes <djsmedes@gmail.com>
2018-08-28 22:12:39 -0400

    basic UI for combatants

---
+++ _assets/src/components/Combatant.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <h5 class="card-header">{{ combatant.name }}</h5>
+    <div class="card-body">
+      <p class="card-text">
+        buffs, debuffs, and effects go here
+      </p>
+    </div>
+    <div class="card-footer">
+      <small>
+        <span class="oi oi-timer" title="initiative" aria-hidden="true"></span>
+        {{ combatant.initiative }}
+      </small>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Combatant",
+    props: {
+      combatant: Object
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ _assets/src/views/Combat.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card-deck">
+  <template v-for="(combatant, uuid) in combatants">
+    <combatant :combatant="combatant"/>
+  </template>
+  </div>
+</template>
+
+<script>
+  import Combatant from '@/components/Combatant'
+
+  export default {
+    name: "Combat",
+    components: {
+      Combatant
+    },
+    data() {
+      return {
+        combatants: {
+          1: {
+            name: 'Berion Garfunkel',
+            initiative: 14
+          },
+          2: {
+            name: 'Elycia Greenfield',
+            initiative: 10
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
