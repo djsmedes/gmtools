2324f454163951db3f6d648db34abbe613033e8f
djsmedes <djsmedes@gmail.com>
2018-08-29 00:02:48 -0400

    frontend has basic access to backend

--- _assets/src/api.js
+++ _assets/src/api.js
@@ -1,32 +1,81 @@
 import axios from 'axios'
+import Vue from 'vue'
 
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
+function generateUrl (model, uuid = null) {
+  const baseStr = '/api/' + model + '/';
+  if (uuid) {
+    return baseStr + uuid + '/';
+  } else {
+    return baseStr;
+  }
+}
+
+function array2ObjByUUID (array) {
+  return array.reduce((accumulator, currentVal) => {
+    accumulator[currentVal.uuid] = currentVal;
+    return accumulator
+  }, {})
+}
+
+export const models = {
+  USER: 'user',
+  COMBATANT: 'combatant'
+};
+
+export const actionTypes = {
+  LIST_OBJECTS: 'list_objects',
+  GET_USER: 'get_user'
+};
+
+export const mutationTypes = {
+  SET_OBJECT_LIST: 'set_object_list',
+  SET_USER: 'set_user'
+};
+
 export default {
-  get_user(context) {
-    return axios.get(
-        '/api/request-user/'
-    ).then(r => {
-      context.commit({
-        type: 'set_user',
-        user: r.data
-      })
-    }).catch(e => {
-      console.log(e)
-    })
+  models: models,
+  state: {
+    [models.USER]: {},
+    [models.COMBATANT]: {}
   },
-  get_object(context, { uuid, model }) {
-    return axios.get(uuid + '/', {
-      baseURL: '/api/' + String(model),
-    }).then(r => {
-      context.commit({
-        type: 'set_object',
-        model: model,
-        object: r.data
+  actionTypes: actionTypes,
+  actions: {
+    [actionTypes.GET_USER] (context) {
+      return axios.get(
+          '/api/request-user/'
+      ).then(r => {
+        context.commit({
+          type: mutationTypes.SET_USER,
+          user: r.data
+        })
+      }).catch(e => {
+        console.log(e)
       })
-    }).catch(e => {
-      console.log(e);
-    })
+    },
+    [actionTypes.LIST_OBJECTS] ({commit}, {model}) {
+      return axios.get(
+          generateUrl(model)
+      ).then(r => {
+        commit({
+          type: mutationTypes.SET_OBJECT_LIST,
+          model: model,
+          object_list: array2ObjByUUID(r.data)
+        })
+      }).catch(e => {
+        console.log(e);
+      })
+    }
+  },
+  mutationTypes: mutationTypes,
+  mutations: {
+    [mutationTypes.SET_OBJECT_LIST] (state, { object_list, model }) {
+      Vue.set(state, model, object_list)
+    },
+    [mutationTypes.SET_USER] (state, { user }) {
+      Vue.set(state, 'user', user)
+    }
   }
 }
