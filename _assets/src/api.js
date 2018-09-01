f7e6b75e473acb51437f494ea69542e6495fdca1
djsmedes <djsmedes@gmail.com>
2018-09-01 00:27:45 -0400

    re-enabling token auth

--- _assets/src/api.js
+++ _assets/src/api.js
@@ -27,27 +27,42 @@ export const models = {
 
 export const actionTypes = {
   LIST_OBJECTS: 'list_objects',
-  GET_USER: 'get_user'
+  GET_USER: 'get_user',
+  GET_TOKEN: 'get_token'
 };
 
 export const mutationTypes = {
   SET_OBJECT_LIST: 'set_object_list',
-  SET_USER: 'set_user'
+  SET_USER: 'set_user',
+  SET_AUTH: 'set_auth'
 };
 
 export default {
   models: models,
   state: {
     [models.USER]: {},
-    [models.COMBATANT]: {}
+    [models.COMBATANT]: {},
+    Authorization: ''
   },
   actionTypes: actionTypes,
   actions: {
-    [actionTypes.GET_USER] (context) {
-      return axios.get(
-          '/api/request-user/'
+    [actionTypes.GET_TOKEN] ({ commit }, { username, password }) {
+      return axios.post(
+          '/api/token-auth/', {
+            username: username,
+            password: password
+          }
       ).then(r => {
-        context.commit({
+        commit(mutationTypes.SET_AUTH, { auth_string: 'Token ' + r.data.token })
+      }).catch(e => {
+        console.log(e)
+      })
+    },
+    [actionTypes.GET_USER] ({ state, commit }) {
+      return axios.get(
+          '/api/request-user/', { headers: { Authorization: state.Authorization } }
+      ).then(r => {
+        commit({
           type: mutationTypes.SET_USER,
           user: r.data
         })
@@ -55,9 +70,9 @@ export default {
         console.log(e)
       })
     },
-    [actionTypes.LIST_OBJECTS] ({commit}, {model}) {
+    [actionTypes.LIST_OBJECTS] ({ commit, state }, { model }) {
       return axios.get(
-          generateUrl(model)
+          generateUrl(model), { headers: { Authorization: state.Authorization } }
       ).then(r => {
         commit({
           type: mutationTypes.SET_OBJECT_LIST,
@@ -76,6 +91,9 @@ export default {
     },
     [mutationTypes.SET_USER] (state, { user }) {
       Vue.set(state, 'user', user)
+    },
+    [mutationTypes.SET_AUTH] (state, { auth_string }) {
+      Vue.set(state, 'Authorization', auth_string)
     }
   }
 }
--- _config/settings/base.py
+++ _config/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
     # third party apps
     'rest_framework',
+    'rest_framework.authtoken',
     'rules.apps.AutodiscoverRulesConfig',
     'authtools',
 
@@ -160,6 +161,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
