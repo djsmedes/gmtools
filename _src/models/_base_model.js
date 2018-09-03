29e819905ec8e70b55c7591f55f95a3c95b0d434
djsmedes <djsmedes@gmail.com>
2018-09-03 07:41:27 -0400

    switching to ecmascript 2015 class syntactic sugar

--- _src/models/_base_model.js
+++ _src/models/_base_model.js
@@ -10,67 +10,68 @@ function array2ObjByUUID (array) {
   }, {})
 }
 
-export function ApiVuexModel (modelName) {
-  this.namespace = modelName;
-  this.modelName = modelName;
-  this.getterTypes = {
-    IDS: this.modelName + 'IDs',
-    LIST: this.modelName + 'List',
-    BY_ID: this.modelName + 'ByID'
-  };
-  this.actionTypes = {
-    CREATE: 'create',
-    RETRIEVE: 'retrieve',
-    UPDATE: 'update',
-    DESTROY: 'destroy',
-    LIST: 'list',
-    REFRESH: 'refresh',
-    REFRESH_LIST: 'refreshList',
-    OPTIONS: 'options'
-  };
-  this.mutationTypes = {
-    SET_LIST: 'setList',
-    SET: 'set',
-    REMOVE: 'remove',
-    SET_OPTIONS: 'setOptions'
-  };
-  this.store = {
-    namespaced: true,
-    state: {
-      [this.modelName]: {}
-    },
-    getters: {
-      [this.getterTypes.IDS]: state => {
-        return Object.keys(state[modelName])
+export class ApiVuexModel {
+  constructor (modelName) {
+    this.namespace = modelName;
+    this.modelName = modelName;
+    this.getterTypes = {
+      IDS: this.modelName + 'IDs',
+      LIST: this.modelName + 'List',
+      BY_ID: this.modelName + 'ByID'
+    };
+    this.actionTypes = {
+      CREATE: 'create',
+      RETRIEVE: 'retrieve',
+      UPDATE: 'update',
+      DESTROY: 'destroy',
+      LIST: 'list',
+      REFRESH: 'refresh',
+      REFRESH_LIST: 'refreshList',
+      OPTIONS: 'options'
+    };
+    this.mutationTypes = {
+      SET_LIST: 'setList',
+      SET: 'set',
+      REMOVE: 'remove',
+      SET_OPTIONS: 'setOptions'
+    };
+    this.store = {
+      namespaced: true,
+      state: {
+        [this.modelName]: {}
       },
-      [this.getterTypes.LIST]: (state, getters) => {
-        return getters[this.getterTypes.IDS].map(
-            ID => Object.assign(state[modelName][ID], { uuid: ID })
-        )
+      getters: {
+        [this.getterTypes.IDS]: state => {
+          return Object.keys(state[modelName])
+        },
+        [this.getterTypes.LIST]: (state, getters) => {
+          return getters[this.getterTypes.IDS].map(
+              ID => Object.assign(state[modelName][ID], { uuid: ID })
+          )
+        },
+        [this.getterTypes.BY_ID]: state => uuid => state[modelName][uuid]
       },
-      [this.getterTypes.BY_ID]: state => uuid => state[modelName][uuid]
-    },
-    actions: {
-      [this.actionTypes.LIST]: ({ state, commit, rootState }) => {
-        if (typeof state[modelName] === 'undefined' || _.isEmpty(state[modelName])) {
-          return api.listObjects({ model: this.modelName, axiosConfig: { headers: { Authorization: rootState.Authorization } } }, objList => {
-            commit(this.mutationTypes.SET_LIST, {
-              objList: array2ObjByUUID(objList)
+      actions: {
+        [this.actionTypes.LIST]: ({ state, commit, rootState }) => {
+          if (typeof state[modelName] === 'undefined' || _.isEmpty(state[modelName])) {
+            return api.listObjects({
+              model: this.modelName,
+              axiosConfig: { headers: { Authorization: rootState.Authorization } }
+            }, objList => {
+              commit(this.mutationTypes.SET_LIST, {
+                objList: array2ObjByUUID(objList)
+              })
             })
-          })
-        } else {
-          return Promise.resolve()
+          } else {
+            return Promise.resolve()
+          }
+        }
+      },
+      mutations: {
+        [this.mutationTypes.SET_LIST]: (state, { objList }) => {
+          Vue.set(state, this.modelName, objList)
         }
       }
-    },
-    mutations: {
-      [this.mutationTypes.SET_LIST]: (state, { objList }) => {
-        Vue.set(state, this.modelName, objList)
-      }
-    }
-  };
+    };
+  }
 }
-
-ApiVuexModel.prototype = {
-  constructor: ApiVuexModel,
-};
--- _src/models/combatant.js
+++ _src/models/combatant.js
@@ -2,18 +2,27 @@ import { ApiVuexModel } from './_base_model';
 
 const modelName = 'combatant';
 
-export function Combatant({ uuid='', name='', initiative=0, hp=0 }={uuid: '', name: '', initiative: 0, hp: 0}) {
-  this.uuid = uuid;
-  this.name = name;
-  this.initiative = initiative;
-  this.hp = hp;
+export class Combatant {
+  constructor ({
+                 uuid = '',
+                 name = '',
+                 initiative = 0,
+                 hp = 0
+               } = { uuid: '', name: '', initiative: 0, hp: 0 }) {
+    this.uuid = uuid;
+    this.name = name;
+    this.initiative = initiative;
+    this.hp = hp;
+  }
 }
 
-Combatant.prototype = {
-  constructor: Combatant,
-};
+class ApiVuexCombatant extends ApiVuexModel {
+  constructor() {
+    super(modelName);
+  }
+}
 
 export default {
   Combatant,
-  ...(new ApiVuexModel(modelName))
+  ...(new ApiVuexCombatant())
 }
