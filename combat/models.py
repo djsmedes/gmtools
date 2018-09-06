36b1c77cfb6c273079c2b0241cc73e22d5f1ba0a
djsmedes <djsmedes@gmail.com>
2018-09-06 09:35:18 -0400

    adding effects field in combatant model

--- combat/models.py
+++ combat/models.py
@@ -8,6 +8,7 @@ class Combatant(TenantModel):
     initiative = models.IntegerField(null=True, blank=True)
     hp = models.IntegerField(null=True, blank=True)
     loot = models.TextField(null=True, blank=True)
+    effects = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
