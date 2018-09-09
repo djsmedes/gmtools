a32b117dcce377d186cabde05c6ff2b454844606
djsmedes <djsmedes@gmail.com>
2018-09-09 12:23:42 -0400

    removing unnecessary extra api action

--- accounts/viewsets.py
+++ accounts/viewsets.py
@@ -3,7 +3,6 @@ from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, UpdateModelMixin
 from rest_framework.response import Response
 from rest_framework.status import HTTP_403_FORBIDDEN
-from rest_framework.decorators import action
 
 from .models import User, Campaign
 from .serializers import UserSerializer, CampaignSerializer
@@ -19,13 +18,6 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
         if request.user == self.get_object():
             return super().update(request, *args, **kwargs)
         return Response({'detail': 'Users can only edit themselves.'}, status=HTTP_403_FORBIDDEN)
-
-    @action(methods=['post'], detail=True)
-    def set_active_campaign(self, request, slug=None, *args, **kwargs):
-        print(slug)
-        print(args)
-        print(kwargs)
-        return Response({})
 
 
 class CampaignViewSet(ModelViewSet):
