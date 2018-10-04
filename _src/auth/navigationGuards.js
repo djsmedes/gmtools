e57f05c8c705ff6944d73ec448f7d2eab0b42e78
djsmedes <djsmedes@gmail.com>
2018-10-04 14:11:06 -0400

    cleaning up dev style on navigation guards

--- _src/auth/navigationGuards.js
+++ _src/auth/navigationGuards.js
@@ -2,36 +2,26 @@ import store from "@/store";
 import auth from "@/auth";
 import { routeNames } from "@/router";
 
+const wasUserRequested = () =>
+  store.getters[
+    auth.namespace + "/" + auth.getterTypes.WAS_AUTH_USER_REQUESTED
+  ];
+
+const isUserAuthenticated = () =>
+  store.getters[auth.namespace + "/" + auth.getterTypes.IS_USER_AUTHENTICATED];
+
 export async function userRequired(to, from, next) {
-  if (
-    !store.getters[
-      auth.namespace + "/" + auth.getterTypes.WAS_AUTH_USER_REQUESTED
-    ]
-  ) {
-    await store.dispatch(auth.namespace + "/" + auth.actionTypes.GET_USER);
-  }
-  next();
+  wasUserRequested()
+    ? next()
+    : await store.dispatch(auth.namespace + "/" + auth.actionTypes.GET_USER);
 }
 
 export function loginRequired(to, from, next) {
-  if (
-    store.getters[auth.namespace + "/" + auth.getterTypes.IS_USER_AUTHENTICATED]
-  ) {
-    next();
-  } else {
-    next({
-      name: routeNames.LOGIN,
-      query: { next: to.path }
-    });
-  }
+  isUserAuthenticated()
+    ? next()
+    : next({ name: routeNames.LOGIN, query: { next: to.path } });
 }
 
-export async function loggedInExcluded(to, from, next) {
-  if (
-    store.getters[auth.namespace + "/" + auth.getterTypes.IS_USER_AUTHENTICATED]
-  ) {
-    next({ name: routeNames.HOME });
-  } else {
-    next();
-  }
+export function loggedInExcluded(to, from, next) {
+  !isUserAuthenticated() ? next() : next({ name: routeNames.HOME });
 }
