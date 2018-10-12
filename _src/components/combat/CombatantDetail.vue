1f1f055d763edab34557122aa671cb4df6c4119f
Daniel Smedema <djsmedes@gmail.com>
2018-10-12 19:11:13 -0400

    adding basic ability to edit and delete combatants

--- _src/components/combat/CombatantDetail.vue
+++ _src/components/combat/CombatantDetail.vue
@@ -1,24 +1,42 @@
 <template>
-  <div>
-    coming soon...
-    <br>
-    {{ JSONcombatant }}
-  </div>
+  <object-detail :title="combatant.name"
+                 :save-func="save"
+                 :clear-func="reset"
+                 :delete-func="deleteSelf">
+    <template slot="view">
+      <div class="card-body">
+        <h5>Name:</h5>
+        <p>{{ combatant.name }}</p>
+      </div>
+    </template>
+    <template slot="edit">
+      <div class="card-body">
+        <form novalidate @submit.stop.prevent>
+          <div class="form-group">
+            <label for="editName">Name</label>
+            <input id="editName" class="form-control" v-model="localCombatant.name">
+          </div>
+        </form>
+      </div>
+    </template>
+  </object-detail>
 </template>
 
 <script>
+import ObjectDetail from "@/components/generic/ObjectDetail";
 import { mapGetters, mapActions } from "vuex";
-import combatant from "@/models/combatant";
+import combatant, { Combatant } from "@/models/combatant";
+import { routeNames } from "@/router";
 
 export default {
   name: "CombatantDetail",
+  components: { ObjectDetail },
   data() {
-    return {};
+    return {
+      localCombatant: new Combatant()
+    };
   },
   computed: {
-    JSONcombatant() {
-      return JSON.stringify(this.combatant);
-    },
     combatant() {
       return this.getCombatant(this.$route.params.uuid);
     },
@@ -28,11 +46,24 @@ export default {
   },
   methods: {
     ...mapActions(combatant.namespace, {
-      loadCombatants: combatant.actionTypes.LIST
-    })
+      loadCombatants: combatant.actionTypes.LIST,
+      deleteCombatant: combatant.actionTypes.DESTROY,
+      updateCombatant: combatant.actionTypes.UPDATE
+    }),
+    async deleteSelf() {
+      await this.deleteCombatant(this.combatant.uuid);
+      this.$router.push({ name: routeNames.COMBATANTS });
+    },
+    async save() {
+      await this.updateCombatant(this.localCombatant);
+      this.reset();
+    },
+    reset() {
+      this.localCombatant = new Combatant(this.combatant);
+    }
   },
   created() {
-    this.loadCombatants();
+    this.loadCombatants().then(() => this.reset());
   }
 };
 </script>
