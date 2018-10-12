5c1260f87b702b4fce6f9e68447daf507adfde2a
Daniel Smedema <djsmedes@gmail.com>
2018-10-12 11:36:10 -0400

    campaign creation page

--- _src/components/accountManage/CampaignCreate.vue
+++ _src/components/accountManage/CampaignCreate.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>
-    Campaign Create
-  </div>
+  <form @submit.prevent="doCreateCampaign" novalidate>
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input id="name" v-model="campaign.name" placeholder="Name" class="form-control"/>
+    </div>
+    <button type="submit" class="btn btn-primary">Create</button>
+  </form>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+import campaign from "@/models/campaign";
+import userModule from "@/models/user";
+import auth from "@/auth";
+import { routeNames } from "@/router";
+
 export default {
-  name: "CampaignCreate"
+  name: "CampaignCreate",
+  data() {
+    return {
+      campaign: new campaign.Campaign()
+    };
+  },
+  computed: {
+    ...mapGetters(auth.namespace, {
+      authUser: auth.getterTypes.AUTH_USER
+    })
+  },
+  methods: {
+    ...mapActions(campaign.namespace, {
+      createCampaign: campaign.actionTypes.CREATE
+    }),
+    ...mapActions(userModule.namespace, {
+      refreshUser: userModule.actionTypes.REFRESH
+    }),
+    async doCreateCampaign() {
+      await this.createCampaign(this.campaign);
+      await this.refreshUser(this.authUser);
+      this.$router.push({ name: routeNames.CAMPAIGNS });
+    }
+  }
 };
 </script>
