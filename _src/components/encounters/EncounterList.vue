4c0660960b6352c6827bf581814470f9a5ff51a1
Daniel Smedema <djsmedes@gmail.com>
2018-10-18 23:59:41 -0400

    have frontend list working i think but its too smart for me

--- _src/components/encounters/EncounterList.vue
+++ _src/components/encounters/EncounterList.vue
@@ -1,9 +1,37 @@
 <template>
-  <div></div>
+  <object-list
+      :detail-view-name="routeNames.ENCOUNTER"
+      :create-view-name="routeNames.ENCOUNTER_CREATE"
+      :object-list="encounters">
+  </object-list>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+import ObjectList from "@/components/generic/ObjectList";
+import { routeNames } from "@/router";
+import encounter from "@/models/encounter";
+
 export default {
-  name: "EncounterList"
+  name: "EncounterList",
+  components: { ObjectList },
+  data() {
+    return {
+      routeNames
+    };
+  },
+  computed: {
+    ...mapGetters(encounter.namespace, {
+      encounters: encounter.getterTypes.LIST
+    })
+  },
+  methods: {
+    ...mapActions(encounter.namespace, {
+      loadEncounters: encounter.actionTypes.LIST
+    })
+  },
+  created() {
+    this.loadEncounters();
+  }
 };
 </script>
