355bb9611ccf4aa69ac5c49044db8632490cae84
Daniel Smedema <djsmedes@gmail.com>
2018-11-01 21:09:17 -0400

    adding completion date to encounter

--- plot/models.py
+++ plot/models.py
@@ -6,6 +6,7 @@ from core.models import CampaignOwnedModel
 class Encounter(CampaignOwnedModel):
 
     name = models.CharField(max_length=127)
+    completion_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
