cd1dea323073c6622697094c9fa48c2a4ac96da8
Daniel Smedema <djsmedes@gmail.com>
2019-02-16 01:58:21 -0500

    adding query list to frontend

--- _src/components/encounters/EncounterChooser.vue
+++ _src/components/encounters/EncounterChooser.vue
@@ -32,7 +32,7 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 import encounter from "@/models/encounter";
 import auth from "@/auth";
 
@@ -41,32 +41,44 @@ export default {
   props: {
     reset: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
   data() {
     return {
       selectedEncounter: null,
-      showCompleted: false
+      showCompleted: false,
+      lazyLoadedCompleted: false,
     };
   },
   watch: {
     reset(newVal) {
       if (newVal) this.selectedEncounter = this.currentEncounter;
-    }
+    },
+    showCompleted(val) {
+      if (!this.lazyLoadedCompleted && val) {
+        this.loadEncountersQuery({ completed_after: "0001-01-01" });
+        this.lazyLoadedCompleted = true;
+      }
+    },
   },
   computed: {
     ...mapGetters(encounter.namespace, {
       encounters: encounter.getterTypes.LIST,
       completedEncounters: encounter.getterTypes.LIST_COMPLETED,
-      getEncounter: encounter.getterTypes.BY_ID
+      getEncounter: encounter.getterTypes.BY_ID,
     }),
     ...mapGetters(auth.namespace, {
-      currentCampaign: auth.getterTypes.CURRENT_CAMPAIGN
+      currentCampaign: auth.getterTypes.CURRENT_CAMPAIGN,
     }),
     currentEncounter() {
       return this.getEncounter(this.currentCampaign.active_encounter);
-    }
-  }
+    },
+  },
+  methods: {
+    ...mapActions(encounter.namespace, {
+      loadEncountersQuery: encounter.actionTypes.QUERY_LIST,
+    }),
+  },
 };
 </script>
