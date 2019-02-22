3f03ab6e21f1befff18499eb26bf615834d76ea0
Daniel Smedema <djsmedes@gmail.com>
2019-02-21 19:23:05 -0500

    adding invitation model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.conf import settings
 from authtools.models import AbstractEmailUser
@@ -91,3 +92,75 @@ class CampaignRole(models.Model):
         on_delete=models.CASCADE
     )
     is_gm = models.BooleanField(default=False)
+
+
+class Invitation(models.Model):
+    """Stores information during the process of both parties
+    approving a link between a User and an Account."""
+
+    EXPIRE_TIMEDELTA = datetime.timedelta(days=7)
+
+    class Meta:
+        unique_together = ('joiner', 'campaign', 'creation_date',)
+
+    uuid = SmallUUIDField(editable=False, default=uuid_default())
+    creation_date = models.DateField(auto_now_add=True)
+    completion_date = models.DateField(editable=False, null=True, blank=True)
+    campaign = models.ForeignKey(
+        'accounts.Campaign',
+        on_delete=models.CASCADE
+    )
+    joiner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    approver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='invites_approved',
+    )
+    joiner_assumed_email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.joiner_assumed_email} -> {self.campaign!s}"
+
+    class Expired(Exception):
+        pass
+
+    class ApproverNotAuthorized(Exception):
+        pass
+
+    def _validate(self, approving_user, accepting_user):
+        assert approving_user, accepting_user
+        if not approving_user.campaigns.filter(campaignrole__is_gm=True).filter(id=self.campaign_id):
+            raise self.ApproverNotAuthorized
+        if datetime.date.today() > self.creation_date + self.EXPIRE_TIMEDELTA:
+            raise self.Expired
+
+    def accept(self, accepting_user=None):
+        self._validate(self.approver, accepting_user or self.joiner)
+        self._complete(self.approver, accepting_user or self.joiner)
+
+    def approve(self, approving_user):
+        self._validate(approving_user, self.joiner)
+        self._complete(approving_user, self.joiner)
+
+    def _complete(self, approving_user, accepting_user):
+        self.approver = approving_user
+        self.joiner = accepting_user
+        self.completion_date = datetime.date.today()
+        self.save()
+        CampaignRole.objects.create(user=accepting_user, campaign=self.campaign)
+
+    @classmethod
+    def get_current_invites(cls):
+        earliest_allowed_creation_date = datetime.date.today() - datetime.timedelta(days=7)
+        return cls.objects.filter(
+            creation_date__gt=earliest_allowed_creation_date
+        ).filter(
+            completion_date__isnull=True
+        )
