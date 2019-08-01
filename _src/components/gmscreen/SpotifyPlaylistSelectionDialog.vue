3c1c5bbc238589de8c607d4593b5275ebabda450
Daniel Smedema <djsmedes@gmail.com>
2019-08-01 11:38:00 -0400

    greatly simplifying and improving code, allowing playlists you don't subscribe to to be added

--- _src/components/gmscreen/SpotifyPlaylistSelectionDialog.vue
+++ _src/components/gmscreen/SpotifyPlaylistSelectionDialog.vue
@@ -19,64 +19,58 @@
             style="overflow: auto; max-height: calc(100vh - 300px)"
           >
             <v-container grid-list-md py-0>
-              <v-item-group v-model="selectedIndices" multiple>
-                <v-data-iterator
-                  :items="playlists"
-                  :rows-per-page-items="[-1]"
-                  hide-actions
-                  content-tag="v-layout"
-                  wrap
-                >
-                  <template #item="{ item }">
-                    <v-flex xs12 sm6 md4 lg3 xl2>
-                      <v-hover>
-                        <template #default="{ hover }">
-                          <v-item>
-                            <template #default="{ active, toggle }">
-                              <v-card
-                                :class="{
-                                  'elevation-0': !hover,
-                                  'elevation-5': hover,
-                                }"
-                                :color="
-                                  active ? 'primary lighten-2' : undefined
-                                "
-                                @click="toggle"
-                              >
-                                <div class="pt-2 mx-2">
-                                  <v-img
-                                    :src="getImgSrc(item)"
-                                    aspect-ratio="1"
-                                  ></v-img>
-                                </div>
-                                <v-card-title class="font-weight-medium">
-                                  {{ item.name }}
-                                </v-card-title>
-                              </v-card>
-                            </template>
-                          </v-item>
-                        </template>
-                      </v-hover>
-                    </v-flex>
-                  </template>
-                  <template #footer v-if="!areAllLoaded">
-                    <v-fade-transition mode="out-in">
-                      <v-progress-linear
-                        indefinite
-                        v-if="loadingFromSpotify"
-                      ></v-progress-linear>
-                      <v-tooltip v-else bottom>
-                        <template #activator="{ on }">
-                          <v-btn v-on="on" flat block @click="loadMore">
-                            <v-icon>expand_more</v-icon>
-                          </v-btn>
-                        </template>
-                        Load more
-                      </v-tooltip>
-                    </v-fade-transition>
-                  </template>
-                </v-data-iterator>
-              </v-item-group>
+              <v-data-iterator
+                :items="playlists"
+                :rows-per-page-items="[-1]"
+                hide-actions
+                content-tag="v-layout"
+                wrap
+              >
+                <template #item="{ item }">
+                  <v-flex xs12 sm6 md4 lg3 xl2>
+                    <v-hover>
+                      <template #default="{ hover }">
+                        <v-card
+                          :class="{
+                            'elevation-0': !hover,
+                            'elevation-5': hover,
+                          }"
+                          :color="
+                            isSelected(item) ? 'primary lighten-2' : undefined
+                          "
+                          @click="toggleSelected(item)"
+                        >
+                          <div class="pt-2 mx-2">
+                            <v-img
+                              :src="getImgSrc(item)"
+                              aspect-ratio="1"
+                            ></v-img>
+                          </div>
+                          <v-card-title class="font-weight-medium">
+                            {{ item.name }}
+                          </v-card-title>
+                        </v-card>
+                      </template>
+                    </v-hover>
+                  </v-flex>
+                </template>
+                <template #footer v-if="!areAllLoaded">
+                  <v-fade-transition mode="out-in">
+                    <v-progress-linear
+                      indefinite
+                      v-if="loadingFromSpotify"
+                    ></v-progress-linear>
+                    <v-tooltip v-else bottom>
+                      <template #activator="{ on }">
+                        <v-btn v-on="on" flat block @click="loadMore">
+                          <v-icon large>expand_more</v-icon>
+                        </v-btn>
+                      </template>
+                      Load more
+                    </v-tooltip>
+                  </v-fade-transition>
+                </template>
+              </v-data-iterator>
             </v-container>
           </v-flex>
           <v-flex
@@ -104,7 +98,7 @@
               Selected
             </h4>
             <v-list style="background: transparent;">
-              <v-hover v-for="(item, index) in selectedSorted" :key="index">
+              <v-hover v-for="(item, index) in sortedSelected" :key="index">
                 <template #default="{ hover }">
                   <v-list-tile
                     :class="{ 'mt-1': index }"
@@ -127,7 +121,7 @@
                       icon
                       flat
                       v-if="hover"
-                      @click="removeFromSelectedById(item.id)"
+                      @click="removeSelected(item)"
                     >
                       <v-icon>clear</v-icon>
                     </v-btn>
@@ -146,7 +140,7 @@
         <v-spacer></v-spacer>
         <v-btn
           :disabled="!isSelectionChanged"
-          @click="close(selectedSorted)"
+          @click="close(sortedSelected)"
           color="save"
           flat
         >
@@ -161,8 +155,7 @@
 <script>
 import { functionalDialogMixin } from "@/mixins";
 import xorBy from "lodash/xorBy";
-import difference from "lodash/difference";
-import findIndex from "lodash/findIndex";
+import sortBy from "lodash/sortBy";
 
 export default {
   name: "SpotifyPlaylistSelectionDialog",
@@ -182,31 +175,9 @@ export default {
       console,
       playlists: [],
       totalPlaylistCount: null, // starts as null instead of 0 so we know if we've tried
-      selectedIndices: [],
       selected: [...this.preSelected],
       loadingFromSpotify: false,
     };
-  },
-  watch: {
-    selectedIndices: {
-      handler(newVal, oldVal) {
-        let newIndices = difference(newVal, oldVal);
-        let removingIndices = difference(oldVal, newVal);
-        for (let index of newIndices) {
-          let newPlaylist = this.playlists[index];
-          if (!this.selectedIds.includes(newPlaylist)) {
-            this.selected.push(newPlaylist);
-          }
-        }
-        for (let index of removingIndices) {
-          let removingPlaylistId = this.playlists[index].id;
-          let removingPlaylistSelectedIndex = findIndex(this.selected, {
-            id: removingPlaylistId,
-          });
-          this.selected.splice(removingPlaylistSelectedIndex, 1);
-        }
-      },
-    },
   },
   computed: {
     sizeAttrs() {
@@ -239,35 +210,38 @@ export default {
     isSelectionChanged() {
       return Boolean(xorBy(this.preSelected, this.selected, "id").length);
     },
-    selectedSorted() {
-      let selected = [...this.selected];
-      let output = [];
-      for (let playlist of this.playlists) {
-        let selectedIndex = findIndex(selected, { id: playlist.id });
-        if (selectedIndex !== -1) {
-          selected.splice(selectedIndex, 1);
-          output.push(playlist);
-        }
-      }
-      return [...output, ...selected];
+    sortedSelected() {
+      return sortBy(this.selected, item => item.name.toLowerCase());
     },
   },
   created() {
     this.loadMore();
   },
   methods: {
+    isSelected(playlist) {
+      return this.selectedIds.includes(playlist.id);
+    },
+    toggleSelected(playlist) {
+      let index = this.selectedIds.indexOf(playlist.id);
+      if (index === -1) {
+        this.selected.push(playlist);
+      } else {
+        this.selected.splice(index, 1);
+      }
+    },
+    removeSelected(playlist) {
+      let index = this.selectedIds.indexOf(playlist.id);
+      if (index !== -1) {
+        this.selected.splice(index, 1);
+      }
+    },
     async loadMore() {
       this.loadingFromSpotify = true;
       let { data } = await this.request({
         method: "get",
         url: `/me/playlists?limit=12&offset=${this.playlists.length}`,
       });
-      for (let playlist of data.items) {
-        this.playlists.push(playlist);
-        if (this.selectedIds.includes(playlist.id)) {
-          this.selectedIndices.push(this.playlists.length - 1);
-        }
-      }
+      this.playlists = this.playlists.concat(data.items);
       this.totalPlaylistCount = data.total;
       this.loadingFromSpotify = false;
     },
@@ -276,38 +250,14 @@ export default {
       let chosenImage = good || tooBig || tooSmall || { url: "" };
       return chosenImage.url;
     },
-    removeFromSelectedById(playlistId) {
-      let playlistIndex = findIndex(this.playlists, { id: playlistId });
-      if (playlistIndex !== -1) {
-        let selectionIndex = this.selectedIndices.indexOf(playlistIndex);
-        if (selectionIndex !== -1) {
-          // the fact that this doesn't trigger the watch is good here - we have to manually
-          //   remove not-yet-loaded ones anyway, so might as well do it all manually
-          this.selectedIndices.splice(selectionIndex, 1);
-        }
-      }
-
-      let selectionIndex = findIndex(this.selected, { id: playlistId });
-      if (selectionIndex !== -1) {
-        this.selected.splice(selectionIndex, 1);
-      }
-    },
     async drop($event) {
       $event.target.classList.remove("drag-over__active");
       let playlistId = await this.getDraggedPlaylistId($event);
       if (playlistId) {
-        // if playlistId already in selected, do nothing
-
-        // if not in selected, but is in candidate list, just add it
-
-        // if in neither, add it with .manual = true; these will be displayed separately
-
-        // when new playlists are loaded, check if any of the .manual ones have those ids
-        //   if so, remove .manual
-
         let playlist = await this.getPlaylist(playlistId);
-        console.log(playlist);
-        playlist.manual = true;
+        if (playlist) {
+          this.selected.push(playlist);
+        }
       }
     },
     dragEnter({ target }) {
